4f21128b3ff4d2d90e3ebd59e17abc1866564319
Jose Alvarez <58921600+josea4599@users.noreply.github.com>
2026-06-01 23:06:29 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 const footer = document.createElement("footer");
 footer.textContent = "Jose A's Portfolio";
-document.body.appendChild(footer);
 let today = new Date();
 let thisYear = today.getFullYear();
 const copyright = document.createElement("p");
-copyright.textContent = "Jose J Alvarez " + thisYear;
-document.body.appendChild(copyright);
+copyright.textContent = `Jose J Alvarez ${thisYear}`;
+footer.appendChild(copyright);
+document.body.appendChild(footer);
 const skills = ["JavaScript", "HTML", "CSS", "GitHub"];
 const skillsList = document.querySelector("#Skills ul");
 for (let i = 0; i < skills.length; i++) {
